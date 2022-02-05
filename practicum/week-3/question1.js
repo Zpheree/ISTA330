4b30ad3983348bb1183993a7834f1b56696b192f
Tyriq Zvijer <tzvijer12@email.arizona.edu>
2022-02-05 15:28:16 -0700

    a

--- practicum/week-3/question1.js
+++ practicum/week-3/question1.js
@@ -2,7 +2,7 @@
 Given an array of numbers, input, 
 find whether the largest element in the array 
 is at least twice as much as every other number
- in the array. If this is the case return true otherwise return false.
+in the array. If this is the case return true otherwise return false.
 
  Example:
          input: [3,2,21,50]
@@ -10,5 +10,22 @@ is at least twice as much as every other number
 */
 
 var largestNumberIsAtLeastTwice = function(input) {
-   
+        let x = Math.max.apply(Math, input);
+        let index = input.indexOf(x);
+
+        if (index > -1) {
+                input.splice(index, 1);
+        }
+
+        for (let element of input) {
+                if (x > (element * 2)) {
+                        continue
+                } else {
+                        return false
+                }
+        }
+        return true
 };
+
+/*let test = largestNumberIsAtLeastTwice([3,2,1,5])
+console.log(test)*/
