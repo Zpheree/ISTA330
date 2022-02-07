f938ca06787595dbcab031dac6edcc37c7c4d837
Tyriq Zvijer <tyriq811@gmail.com>
2022-02-06 20:22:47 -0700

    a

--- practicum/week-3/question10.js
+++ practicum/week-3/question10.js
@@ -16,5 +16,32 @@ output: [[-5, -4], [23, 24]]
 */
 
 var minPairs = function(input) {
+    let retVal = [];
+    let len = 0;
+    for (let i = 0; i < input.length; i++) {
+        let min = i;
+        for (let j = i + 1; j < input.length; j++) {
+            if (input[j] < input[min]) {
+            min = j;
+            }
+        }
+        if (min != i) {
+            [input[i],input[min]] = [input[min],input[i]];
+    }
+    len += 1;
+  }
 
+  let minDif = input[len-1];
+  for (let l = 1; l < input.length; l++) {
+    if (input[l] - input[l-1] < minDif) {
+      retVal = [];
+      minDif = input[l] - input[l-1];
+      retVal.push([input[l-1],input[l]]);
+    } else if (input[l] - input[l-1] == minDif) {
+      retVal.push([input[l-1],input[l]]);
+    }
+  }
+  return retVal;
 };
+
+console.log(minPairs([1,-5,-10,24,19,-4,-14,23]))
